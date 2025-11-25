39229dc5944802c21f9f7aaeb82d4530a7ea72cc
weloskyyy <juan.welosky@gmail.com>
2025-11-24 20:25:21 -0400

    base inicial CRUD Contactos

---
+++ CRUDT3/CRUDT3/Models/Contactomodel.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDT3.Models
+{
+    public class Contactomodel
+    {
+        public int IdContacto { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
