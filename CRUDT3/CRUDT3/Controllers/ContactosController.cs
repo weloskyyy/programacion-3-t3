9029c3a039e93c27a0265de6a7f2f2dda306caed
weloskyyy <juan.welosky@gmail.com>
2025-11-24 21:26:48 -0400

    Contacts CRUD using in-memory FakeDb

---
+++ CRUDT3/CRUDT3/Controllers/ContactosController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUDT3.Data;
+using CRUDT3.Models;
+using System.Linq;
+
+namespace CRUDT3.Controllers
+{
+    public class ContactosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(FakeDb.Contactos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contactomodel contacto)
+        {
+            contacto.IdContacto = FakeDb.Contactos.Max(c => c.IdContacto) + 1;
+            FakeDb.Contactos.Add(contacto);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var contacto = FakeDb.Contactos.FirstOrDefault(c => c.IdContacto == id);
+            return View(contacto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contactomodel contacto)
+        {
+            var item = FakeDb.Contactos.FirstOrDefault(c => c.IdContacto == contacto.IdContacto);
+
+            item.Nombre = contacto.Nombre;
+            item.Telefono = contacto.Telefono;
+            item.Correo = contacto.Correo;
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var contacto = FakeDb.Contactos.FirstOrDefault(c => c.IdContacto == id);
+            return View(contacto);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var contacto = FakeDb.Contactos.FirstOrDefault(c => c.IdContacto == id);
+            FakeDb.Contactos.Remove(contacto);
+            return RedirectToAction("Index");
+        }
+    }
+}
