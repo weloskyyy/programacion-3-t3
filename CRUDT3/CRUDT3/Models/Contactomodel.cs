b2bfb9d802b7e8b75e3449c17a05618a34ee150a
weloskyyy <juan.welosky@gmail.com>
2025-11-24 22:19:30 -0400

    Agregar validación al modelo Contactomodel (campos obligatorios y correo válido)

--- CRUDT3/CRUDT3/Models/Contactomodel.cs
+++ CRUDT3/CRUDT3/Models/Contactomodel.cs
@@ -1,11 +1,19 @@
-﻿namespace CRUDT3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDT3.Models
 {
     public class Contactomodel
     {
         public int IdContacto { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El telefono es obligatorio")]
         public string Telefono { get; set; }
 
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Debe ser un correo válido")]
         public string Correo { get; set; }
     }
 }
